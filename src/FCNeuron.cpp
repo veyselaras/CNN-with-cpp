79eb4739ce0cdfcf365644a695629ce8df4794ee
veyselaras <veysel.aras@ogr.sakarya.edu.tr>
2025-07-10 13:22:18 +0300

    Conv for fc layers finished. Next job is CODING BACK PROPAGATION ALGORTIHM

--- src/FCNeuron.cpp
+++ src/FCNeuron.cpp
@@ -5,6 +5,18 @@ public:
 	double inputValue;
 	double reluValue;
 	
+	double *weightMatrice;
+	
+	FCNeuron(int sizeOfWeightMatrice){
+		weightMatrice = new double[sizeOfWeightMatrice];
+	}
+	
+	FCNeuron(){	}
+	
+	~FCNeuron(){
+		delete[] arr;
+	}
+	
 	void updateRelu(){
 		tanh(inputValue);
 	}
--- src/NetworkAdmin.cpp
+++ src/NetworkAdmin.cpp
@@ -1,39 +1,51 @@
 #include <iostream>
 #include <cmath>
 #include <random>
+#include <vector>
 
 #define FILTER_SIZE = 5
 #define FILTER_NUMBER_FIRST = 6
 #define FILTER_NUMBER_SECOND = 16
+#define FILTER_NUMBER_THIRD = 120
 #define FIRST_CONVLAYER_SIZE = 28
 #define FIRST_POOLAYER_SIZE = 14
 #define SECOND_CONVLAYER_SIZE = 10
 #define SECOND_FILTER_SIZE = 10
 #define SECOND_POOLAYER_SIZE = 5
-#define THIRD_CONVLAYER_SIZE = 120
 #define INPUT_SIZE = 32
+
+//YOU MUST START ALL BIAS AT 0 DON'T FORGET!! 
 
 class NetworkAdmin{
 private:
-	//	input
+	//	input matrice
 	double input[INPUT_SIZE][INPUT_SIZE];
 
-	// layer matrixes
+	// convolutional layer matrices
 	ConvNeuron convOneArr[FILTER_NUMBER_FIRST][FIRST_CONVLAYER_SIZE][FIRST_CONVLAYER_SIZE];
 	double convOneBias[FILTER_NUMBER_FIRST];
 	double poolingOneArr[FILTER_NUMBER_FIRST][FIRST_POOLAYER_SIZE][FIRST_POOLAYER_SIZE];
 	ConvNeuron convTwoArr[FILTER_NUMBER_SECOND][SECOND_CONVLAYER_SIZE][SECOND_CONVLAYER_SIZE];
-	double convOneBias[FILTER_NUMBER_SECOND];
-	double poolingOneArr[FILTER_NUMBER_SECOND][SECOND_POOLAYER_SIZE][SECOND_POOLAYER_SIZE];
-	double convThreeArr[THIRD_CONVLAYER_SIZE];
+	double convTwoBias[FILTER_NUMBER_SECOND];
+	double poolingTwoArr[FILTER_NUMBER_SECOND][SECOND_POOLAYER_SIZE][SECOND_POOLAYER_SIZE];
+	double convThreeArr[FILTER_NUMBER_THIRD];
+	double convThreeBias[FILTER_NUMBER_THIRD];
+
+	// fully connected layers matrices
+	std::vector<FCNeuron> fcLayerOne;
+	fcLayerOne.reserve(120);
+	double fcLayerOneBias[120];
 	
-	FCNeuron fcLayerOne[THIRD_CONVLAYER_SIZE];
-	FCNeuron fcLayerTwo[86];
-	FCNeuron fcLayerThree[10];
+	std::vector<FCNeuron> fcLayerTwo;
+	fcLayerTwo.reserve(86);
+	double fcLayerTwoBias[86];
 	
+	std::vector<FCNeuron> fcLayerThree;
+	fcLayerThree.reserve(20);
+	double fcLayerThreeBias[10];
 	
 	// filters
-	double firstLayerFilter[FILTER_NUMBER_FIRST][FILTER_SIZE][FILTER_SIZE];
+	double firstLayerFilter[FILTER_NUMBER_THIRD][FILTER_SIZE][FILTER_SIZE];
 	double secondLayerFilter[FILTER_NUMBER_SECOND][FILTER_SIZE][FILTER_SIZE];
 	double thirdLayerFilter[THIRD_CONVLAYER_SIZE][FILTER_SIZE][FILTER_SIZE];
 	
@@ -48,45 +60,83 @@ private:
 		switch(filterNumber){
 			case 6:
 				for(int i = 0; i < filterNumber; i++){//width
-					for(int j = column; j < column + 5; j++){//row
-						for(int k = row; k < row + 5; k++){//column
+					for(int j = row; j < row + 5; j++){//row
+						for(int k = column; k < column + 5; k++){//column
 							convSum = firstLayerFilter[i][filterRowCounter][filterColumnCounter]*input[j][k];
+							filterColumnCounter++;
 						}
+						filterRowCounter++;
 						filterColumnCounter = 0;
 					}
 					filterRowCounter = 0;
-					convOneArr[i][j][k] = convSum + convOneBias[i];
+					convOneArr[i][row][column].inputValue = convSum + convOneBias[i];
+					convOneArr[i][row][column].updateRelu();
 				}
 				break;
 			
 			case 16:
 				for(int i = 0; i < filterNumber; i++){//width
-					for(int j = column; j < column + 5; j++){//row
-						for(int k = row; k < row + 5; k++){//column
+					for(int j = row; j < row + 5; j++){//row
+						for(int k = column; k < column + 5; k++){//column
 							convSum = secondLayerFilter[i][filterRowCounter][filterColumnCounter]*input[j][k];
+							filterColumnCounter++;
 						}
+						filterRowCounter++;
 						filterColumnCounter = 0;
 					}
 					filterRowCounter = 0;
-					convOneArr[i][j][k] = convSum + convOneBias[i];
+					convTwoArr[i][row][column].inputValue = convSum + convSecondBias[i];
+					convTwoArr[i][row][column].updateRelu();
 				}
 				break;
 			
 			case 120:
 				for(int i = 0; i < filterNumber; i++){//width
-					for(int j = column; j < column + 5; j++){//row
-						for(int k = row; k < row + 5; k++){//column
+					for(int j = row; j < row + 5; j++){//row
+						for(int k = column; k < column + 5; k++){//column
 							convSum = thirdLayerFilter[i][filterRowCounter][filterColumnCounter]*input[j][k];
+							filterColumnCounter++;
 						}
+						filterRowCounter++;
 						filterColumnCounter = 0;
 					}
 					filterRowCounter = 0;
-					convOneArr[i][j][k] = convSum + convOneBias[i];
+					convThreeArr[i].inputValue = convSum + convThreeBias[i];
+					convThreeArr[i].updateRelu();
 				}
 				break;
 			
 			default:
 				break;
+		}
+	}
+	
+	
+	void fcLayerConv(){
+		double convSum = 0;
+		for (int i = 0; i < 120; i++){
+			if(fcLayerOne.size() != 120)
+				fcLayerOne.emplace_back(86);
+			fcLayerOne[i].inputValue = convThreeArr[i].reluValue;
+			fcLayerOne[i].updateRelu();
+		}
+			
+		for (int i = 0; i < 86; i++){
+			if(fcLayerOne.size() != 86)
+				fcLayerTwo.emplace_back(10);
+			for(int j = 0; j < 120; j++)
+				convSum += fcLayerOne[j].reluValue*fcLayerOne[j]->weightMatrice[i];
+			fcLayerTwo[i].inputValue = convSum + fcLayerTwoBias[i];
+			fcLayerTwo[i].updateRelu();
+		}
+			
+		for (int i = 0; i < 10; i++){
+			if(fcLayerOne.size() != 10)
+				fcLayerThree.emplace_back();
+			for(int j = 0; j < 86; j++)
+				convSum += fcLayerTwo[j].reluValue*fcLayerTwo[j]->weightMatrice[i];
+			fcLayerThree[i].inputValue = convSum + fcLayerThreeBias[i];
+			fcLayerThree[i].updateRelu();
 		}
 	}
 	
@@ -96,29 +146,29 @@ private:
 		}
 		else{
 			switch(name){
-				case layerOne:
-					for(i = 0; i < FILTER_NUMBER_FIRST; i++){
-						for(j = 0; j < INPUT_SIZE-FILTER_SIZE+1; j++){
+				case "layerOne":
+					for(int i = 0; i < INPUT_SIZE-FILTER_SIZE+1; i++){
+						for(int j = 0; j < INPUT_SIZE-FILTER_SIZE+1; j++){
 							matriceMult(FILTER_NUMBER_FIRST, i, j);
 						}
 					}
-				case layerTwo:
-					for(i = 0; i < FILTER_NUMBER_SECOND; i++){
-						for(j = 0; j < FIRST_CONVLAYER_SIZE-FILTER_SIZE+1; j++){
+					break;
+				case "layerTwo":
+					for(int i = 0; i < FIRST_CONVLAYER_SIZE-FILTER_SIZE+1; i++){
+						for(int j = 0; j < FIRST_CONVLAYER_SIZE-FILTER_SIZE+1; j++){
 							matriceMult(FILTER_NUMBER_SECOND, i, j);
 						}
 					}
-				case layerThree:
-					
+					break;
+				case "layerThree":
+					matriceMult(FILTER_NUMBER_THIRD, 0, 0);
 				default:
 					cout << "DEFAULT CASE"<<endl;
 					break;
 			}
+			fcLayerConv();
 		}
-		
-		
 	}
-	
 	
 public:
 	
@@ -158,7 +208,7 @@ public:
 	
 	~NetworkAdmin(){
 		
-	};
+	}
 
 
 };
